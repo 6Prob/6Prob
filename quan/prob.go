ee2d4c4afc98f50364f726962336689ed608e6a9
6Prob <lrf20@mails.tsinghua.edu.cn>
2022-08-15 12:09:02 +0800

    bug fixed

--- quan/prob.go
+++ quan/prob.go
@@ -417,7 +417,6 @@ func (pTree *ProbTree) Generate() string {
 		nextEntry := utils.RandBranchFloat(probs)
 		remain.Del(nowNode.divide)
 		newAddr.Set(nowNode.divide, nextEntry)
-		// fmt.Println(remain.GetAll(), newAddr.ToIPv6())
 		if len(remain.GetAll()) == 0 {
 			break
 		}
